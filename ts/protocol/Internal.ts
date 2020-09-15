70609c5e9e8a223f306fdedfc5775301adf15c2d
Holger Benl <hbenl@evandor.de>
2020-09-15 18:51:12 +0200

    npm run build-ts

---
+++ ts/protocol/Internal.ts
@@ -0,0 +1,222 @@
+import {
+  BuildId,
+  RecordingId,
+  TimeStamp
+} from "./Recording";
+import {
+  MimeType
+} from "./Graphics";
+import {
+  Location
+} from "./Debugger";
+import {
+  SessionId
+} from "./Session";
+
+/**
+ * Information about a file based resource. Currently, resources are used for
+ * source maps and original sources.
+ */
+export interface Resource {
+
+  /**
+   * URL of the resource.
+   */
+  url: string;
+
+  /**
+   * Checksum of the resource's contents.
+   */
+  checksum: string;
+}
+
+export interface createRecordingParameters {
+
+  /**
+   * Build Id of the software which produced the recording.
+   */
+  buildId: BuildId;
+}
+
+export interface createRecordingResult {
+
+  /**
+   * Identifier for the recording.
+   */
+  recordingId: RecordingId;
+}
+
+export interface addRecordingDataParameters {
+
+  /**
+   * ID of the recording data is being added to. This recording must have
+   * been produced by a createRecording command previously sent on this
+   * connection.
+   */
+  recordingId: RecordingId;
+
+  /**
+   * Byte offset into the recording's blob of the data being sent.
+   */
+  offset: number;
+
+  /**
+   * Byte length of the data being sent.
+   */
+  length: number;
+}
+
+export interface addRecordingDataResult {
+
+}
+
+export interface addRecordingDescriptionParameters {
+
+  /**
+   * ID of the recording being described.
+   */
+  recordingId: RecordingId;
+
+  /**
+   * Total byte length of the recording.
+   */
+  length: number;
+
+  /**
+   * Duration of the entire recording.
+   */
+  duration: TimeStamp;
+
+  /**
+   * Mime type for graphics data at the end of the recording.
+   */
+  lastScreenMimeType?: MimeType;
+
+  /**
+   * Raw graphics data for the end of the recording.
+   */
+  lastScreenData?: string;
+
+  /**
+   * Any command line arguments associated with the recording.
+   */
+  commandLineArguments?: string[];
+}
+
+export interface addRecordingDescriptionResult {
+
+}
+
+export interface hasResourceParameters {
+
+  /**
+   * Resource to look for.
+   */
+  resource: Resource;
+}
+
+export interface hasResourceResult {
+
+  /**
+   * Whether the resource's contents are known.
+   */
+  known: boolean;
+}
+
+export interface addResourceParameters {
+
+  /**
+   * Resource to add.
+   */
+  resource: Resource;
+
+  /**
+   * Text contents of the resource.
+   */
+  contents: string;
+}
+
+export interface addResourceResult {
+
+}
+
+export interface addRecordingResourceParameters {
+
+  /**
+   * ID of the recording.
+   */
+  recordingId: RecordingId;
+
+  /**
+   * Resource which the recording is associated with.
+   */
+  resource: Resource;
+}
+
+export interface addRecordingResourceResult {
+
+}
+
+export interface getAssertionFiltersParameters {
+
+}
+
+export interface getAssertionFiltersResult {
+
+  filters?: any;
+}
+
+export interface echoParameters {
+
+  str: string;
+
+  count: number;
+}
+
+export interface echoResult {
+
+  str: string;
+}
+
+export interface convertLocationToFunctionOffsetParameters {
+
+  location: Location;
+}
+
+export interface convertLocationToFunctionOffsetResult {
+
+  functionId: string;
+
+  offset: BigInt;
+}
+
+export interface convertFunctionOffsetToLocationParameters {
+
+  functionId: string;
+
+  offset?: BigInt;
+}
+
+export interface convertFunctionOffsetToLocationResult {
+
+  location: Location;
+}
+
+export interface labelTestSessionParameters {
+
+  sessionId: SessionId;
+}
+
+export interface labelTestSessionResult {
+
+}
+
+export interface getRecordingsParameters {
+
+  authId: string;
+}
+
+export interface getRecordingsResult {
+
+  recordings: any[];
+}
---
+++ ts/protocol/Session.ts
@@ -0,0 +1,107 @@
+import {
+  TimeRange,
+  MouseEvent,
+  TimeStampedPoint,
+  ExecutionPoint
+} from "./Recording";
+import {
+  PauseId,
+  CallStack,
+  PauseData
+} from "./Pause";
+
+/**
+ * Unique identifier for a session which a recording has been loaded into.
+ */
+export type SessionId = string;
+
+export interface ensureProcessedParameters {
+
+}
+
+export interface ensureProcessedResult {
+
+}
+
+export interface findMouseEventsParameters {
+
+}
+
+export interface findMouseEventsResult {
+
+}
+
+export interface getEndpointParameters {
+
+}
+
+export interface getEndpointResult {
+
+  endpoint: TimeStampedPoint;
+}
+
+export interface createPauseParameters {
+
+  /**
+   * Point to create the pause at.
+   */
+  point: ExecutionPoint;
+}
+
+export interface createPauseResult {
+
+  /**
+   * Identifier for the new pause.
+   */
+  pauseId: PauseId;
+
+  /**
+   * Stack contents, omitted if there are no frames on the stack at this point.
+   */
+  stack?: CallStack;
+
+  /**
+   * Data describing the frames on the stack and the in scope
+   * variables of the topmost frame.
+   */
+  data: PauseData;
+}
+
+export interface releasePauseParameters {
+
+  pauseId: PauseId;
+}
+
+export interface releasePauseResult {
+
+}
+
+/**
+ * Event describing regions of the recording that have not been uploaded.
+ */
+export interface missingRegions {
+
+  /**
+   * Regions that have not been uploaded.
+   */
+  regions: TimeRange[];
+}
+
+/**
+ * Event describing regions of the recording that have not been processed.
+ */
+export interface unprocessedRegions {
+
+  /**
+   * Any missing regions are also unprocessed regions.
+   */
+  regions: TimeRange[];
+}
+
+/**
+ * Describes some mouse events that occur in the recording.
+ */
+export interface mouseEvents {
+
+  events: MouseEvent[];
+}
